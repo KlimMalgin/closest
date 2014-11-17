4860ebebeb65b71f1f7597e9a2c33b3beb41f7ce
KlimMalgin <fxklim@gmail.com>
2014-11-17 22:58:03 +0500

    comments

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
  */
 'use strict';
 
+/**
+ * Восходящий поиск по DOM-дереву относительно элемента el. Ищет ближайшего родителя с классом targetClass
+ * @param el элемент с которого начинается поиск
+ * @param targetClass класс по которому определяется искомый элемент
+ * @param iter счетчик количества пройденных элементов
+ * @param stop максимально допустимая глубина поиска (по умолчанию === 100)
+ * @returns {DOM Element|null}
+ */
 function getClosestParent (el, targetClass, iter, stop) {
     iter = iter || 1;
     stop = stop || 100;
@@ -12,9 +20,18 @@ function getClosestParent (el, targetClass, iter, stop) {
 }
 
 module.exports = {
+
+    /**
+     * Bool-проверка - существует родитель с заданным классом или нет
+     * @returns {boolean}
+     */
     closest: function () {
         return !!getClosestParent.apply({}, arguments);
     },
+
+    /**
+     * Непосредственно поиск элемента
+     */
     isClosest: getClosestParent
 };
 
